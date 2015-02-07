418df2545e860671848173282d4735c18fccf4a4
Stephen Jasina <stephenmathjasina@gmail.com>
2015-02-07 10:45:17 -0500

    Merging the talon branch with the master branch

--- 5066_2015/src/org/usfirst/frc/team5066/robot/Robot.java
+++ 5066_2015/src/org/usfirst/frc/team5066/robot/Robot.java
@@ -1,11 +1,9 @@
 package org.usfirst.frc.team5066.robot;
 
-import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.RobotDrive;
-
+import edu.wpi.first.wpilibj.Talon;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -15,56 +13,59 @@ import edu.wpi.first.wpilibj.RobotDrive;
  * directory.
  */
 public class Robot extends IterativeRobot {
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-	CANTalon cant2, cant3, cant4, cant5;
-	final double MULTIPLIER = .7;
-	
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+
+	final double MULTIPLIER = .5;
+
 	Joystick js;
-	JoystickButton jsb2, jsb3, jsb4, jsb5;
-	
 	RobotDrive rd;
-	
-    public void robotInit() {
-    	js = new Joystick(0);
-    	cant2 = new CANTalon(2);
-    	cant3 = new CANTalon(3);
-    	cant4 = new CANTalon(4);
-    	cant5 = new CANTalon(5);
+	Talon backLeft, backRight, frontLeft, frontRight;
 
-    	jsb2 = new JoystickButton(js, 2);
-    	jsb3 = new JoystickButton(js, 3);
-    	jsb4 = new JoystickButton(js, 4);
-    	jsb5 = new JoystickButton(js, 5);
-    	
-    	rd = new RobotDrive(cant5, cant2, cant3, cant4);
-    	
-    }
+	public void robotInit() {
+		js = new Joystick(0);
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-    }
+		backLeft = new Talon(2);
+		backRight = new Talon(5);
+		frontLeft = new Talon(6);
+		frontRight = new Talon(7);
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-//    	frontLeft.set((-js.getY() + js.getX() + js.getZ()) * MULTIPLIER * (-js.getThrottle() + 1) / 2);
-//    	backLeft.set((js.getY() + js.getX() + js.getZ()) * MULTIPLIER * (-js.getThrottle() + 1) / 2);
-//    	frontRight.set((-js.getY() + js.getX() + js.getZ()) * MULTIPLIER * (-js.getThrottle() + 1) / 2);
-//    	backRight.set((js.getY() + js.getX() + js.getZ()) * MULTIPLIER * (-js.getThrottle() + 1) / 2
-    	rd.mecanumDrive_Cartesian(js.getX() / 4, -js.getZ() / 4, -js.getY() / 4, 0);
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-    	
-    }
-    
+		rd = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		rd.mecanumDrive_Cartesian(js.getX() * MULTIPLIER, js.getY()
+				* MULTIPLIER, js.getZ() * MULTIPLIER, 0);
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic() {
+
+	}
+
+	public static void driveMecanum(Joystick js, Talon frontRight,
+			Talon backRight, Talon frontLeft, Talon backLeft, double multiplier) {
+		double magnitude = Math.sqrt(js.getX() * js.getX() + js.getY()
+				+ js.getY()) * multiplier,
+				direction = Math.atan(js.getY() / js.getX())
+				+ (js.getX() > 0 ? Math.PI : 0);
+
+		frontLeft.set(magnitude * Math.sin(direction + Math.PI / 4));
+		backLeft.set(magnitude * Math.cos(direction + Math.PI / 4));
+		frontRight.set(magnitude * Math.cos(direction + Math.PI / 4));
+		backRight.set(magnitude * Math.sin(direction   + Math.PI / 4));
+	}
 }
