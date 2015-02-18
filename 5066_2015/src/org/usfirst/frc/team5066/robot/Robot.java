54444947db406591fbbca057f519126b29bc7756
Stephen Jasina <stephenmathjasina@gmail.com>
2015-02-17 21:53:43 -0500

    Added timer to avoid too much data to processor and motors
    Organization of variable declarations and initializations (as well as
    config.properties)
    
    Reread properties added
    
    Potential changes to camera code?
    
    General comments

--- 5066_2015/src/org/usfirst/frc/team5066/robot/Robot.java
+++ 5066_2015/src/org/usfirst/frc/team5066/robot/Robot.java
@@ -2,7 +2,6 @@ package org.usfirst.frc.team5066.robot;
 
 import java.io.IOException;
 import java.util.Properties;
-import java.io.PrintWriter;
 
 import org.salinerobotics.library.SingularityDrive;
 import org.salinerobotics.library.SingularityReader;
@@ -10,12 +9,10 @@ import org.salinerobotics.library.controller.Logitech;
 import org.salinerobotics.library.controller.SingularityController;
 import org.salinerobotics.library.controller.XBox;
 
-import edu.wpi.first.wpilibj.AnalogTrigger;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
@@ -28,110 +25,104 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * In other words: screw with the name, you screw with the manifest
  */
 public class Robot extends IterativeRobot {
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+	// Create a SingularityReader to read the properties file.
+	private SingularityReader sr;
+	private final String PROP_FILE_URL = "/config.properties";
 
-	final double MULTIPLIER = 1;
-
-	// create integers for ports, intakes, and camera
-	private int backLeft, backRight, frontLeft, frontRight, intakeInnerLeft,
-			intakeInnerRight, intakeOuterLeft, intakeOuterRight;
+	// Create stuff for camera
 	private int cameraQuality;
 	private String cameraPort1;
+	CameraServer cs;
 
-	// create ultrasonic object
-	Ultrasonic us;
+	// Create integer for ultrasonic sensor
+	private int rfPort;
 
-	// create input controllers
+	// Create integers for input controllers and settings
 	Joystick movementJoystick, intakeJoystick;
-	SingularityController intakeController;
-	SingularityController movementController;
+	SingularityController intakeController, movementController;
 	XBox xbox;
 	Logitech logitech;
 
 	int controlMode, driveMode;
-	int XBOX_DRIVE = 1, LOGITECH_DRIVE = 0;
+	int LOGITECH_DRIVE = 0, XBOX_DRIVE = 1;
 
-	RobotDrive rd;
-	Intake intake;
-	Camera2015 cam1, cam2;
-	CameraServer cs;
+	// Create integers for TalonSR and TalonSRX ports
+	private int backLeft, backRight, frontLeft, frontRight, intakeInnerLeft,
+			intakeInnerRight, intakeOuterLeft, intakeOuterRight;
 
-	private SingularityDrive sd;
-	private SingularityReader sr;
-	private final String PROP_FILE_URL = "/config.properties";
-	AnalogTrigger at;
-
-	RangeFinder rf;
-	int mode;
-	boolean startWasPressed;
-
-	final double TRANSLATION_CONSTANT = 1, ROTATION_CONSTANT = 0.5;
-	final String[] MODES = { "Mecanum", "Arcade", "Tank" };
-
+	// Create output (elevator, intakes, and chassis drive) objects
 	Elevator elevator;
+	Intake intake;
+	private SingularityDrive sd;
 
+	// These are variables for changing the drive modes
+	// int mode;
+	// boolean startWasPressed;
+	// final String[] MODES = { "Mecanum", "Arcade", "Tank" };
+
+	// Create movement constants. These edit the max speeds.
+	final double TRANSLATION_CONSTANT = 1, ROTATION_CONSTANT = 0.5;
+
+	// Create intake speed constant. This edits the max speed, and is editable
+	// in the SmartDashboard
 	double intakeMultiplier;
 
-	PrintWriter pr;
-
+	// Auton mode variables.
+	double timerDelay;
 	int autonMode;
 
-	// Robot Init
-	// ==========================================================================================
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 * 
+	 * First, we use a SingularityReader to read the properties out of a local
+	 * file (local to the robot). If the read fails, we revert to default values
+	 * (see try-catch-finally block). Next, we initialize more variables, which
+	 * are mostly dependent on the now initialize properties.
+	 */
 	public void robotInit() {
 		sr = new SingularityReader();
 		try {
 			applyProperties(sr.readProperties(PROP_FILE_URL));
 		} catch (IOException e) {
 			SmartDashboard.putString("Properties Loaded", "unsuccessfully");
-			System.out
-					.println("Failed to load properties file, loading defaults");
-
 			// Ports
 
 			frontLeft = 7;
 			backRight = 4;
 			frontRight = 6;
 			backLeft = 5;
-			intakeInnerLeft = 2;
-			intakeInnerRight = 3;
-			intakeOuterLeft = 3; // CANTalon
-			intakeOuterRight = 4; // CANTalon
 
-			us = new Ultrasonic(1, 0);
-			us.setEnabled(true);
+			// CANTalons
+			intakeInnerLeft = 3;
+			intakeInnerRight = 4;
+
+			// TalonSRs
+			intakeOuterLeft = 3;
+			intakeOuterRight = 2;
 
 			// Initialize the camera properties
 			cameraQuality = 50;
 			cameraPort1 = "cam0";
 
-			rf = new RangeFinder(0);
-
 			intakeController = xbox;
 			movementController = logitech;
 
 			autonMode = 0;
+
+			timerDelay = 0.005;
+		} finally {
+			SmartDashboard.putString("Properties Loaded", "successfully");
 		}
+
 		// TODO delete Vision_2015
-		// cam1 = new Camera2015(cameraPort1, cameraQuality);
-		// cam1.initCameraForProcessing();
-		/*
-		 * for cam 2 cam2 = new Camera2015(cameraPort2, cameraQuality);
-		 * cam2.initCameraForProcessing();
-		 */
-		// initialize the intake properties
-		intake = new Intake(intakeOuterLeft, intakeOuterRight, intakeInnerLeft,
-				intakeInnerRight);
+
 		// Initialize the camera, and start taking video
-		// cs = CameraServer.getInstance();
-		// cs.setQuality(cameraQuality);
-		// cs.startAutomaticCapture(cameraPort1);
+		cs = CameraServer.getInstance();
+		cs.setQuality(cameraQuality);
+		cs.startAutomaticCapture(cameraPort1);
 
-		sd = new SingularityDrive(frontLeft, backLeft, frontRight, backRight);
-
+		// Initialize the user inputs.
 		movementJoystick = new Joystick(0);
 		intakeJoystick = new Joystick(1);
 
@@ -144,26 +135,29 @@ public class Robot extends IterativeRobot {
 			movementController = new XBox(intakeJoystick, 0.15);
 		}
 
-		mode = 0;
-		SmartDashboard.putString("Mode", "Mecanum");
-		startWasPressed = false;
+		// Initialize the robot movements.
+		elevator = new Elevator(2, rfPort);
+		intake = new Intake(intakeOuterLeft, intakeOuterRight, intakeInnerLeft,
+				intakeInnerRight);
+		sd = new SingularityDrive(frontLeft, backLeft, frontRight, backRight);
 
-		elevator = new Elevator(2);
+		// Here's some more stuff for different drive modes
+		// mode = 0;
+		// SmartDashboard.putString("Mode", "Mecanum");
+		// startWasPressed = false;
 
-		try {
-			pr = new PrintWriter("inputRead.txt", "UTF-8");
-		} catch (Exception e) {
-			System.exit(1);
-		}
+		// Don't reapply the properties yet, but keep the option open
+		SmartDashboard.putBoolean("Reapply Properties", false);
 	}
 
 	/**
 	 * This function is called periodically during autonomous
+	 * 
+	 * We use a
 	 */
 	public void autonomousPeriodic() {
 		switch (autonMode) {
 		case 1:
-
 			break;
 		case 2:
 			break;
@@ -179,14 +173,13 @@ public class Robot extends IterativeRobot {
 		// SmartDashboard.putNumber("Is enabled",0);
 		// cam.processImages();
 
+		// Keep this if you want to live. Also if you want to have three
+		// drive modes.
 		/*
 		 * if (movementController.getStart()) { if (!startWasPressed) { mode =
 		 * (mode + 1) % 3; SmartDashboard.putString("Mode", MODES[mode]); }
 		 * startWasPressed = true; } else { startWasPressed = false; }
 		 */
-
-		// Keep this if you want to live. Also if you want to have three
-		// (sqrt(!4)) drive modes.
 		/*
 		 * switch (mode) { case 0: sd.driveMecanum(movementController,
 		 * TRANSLATION_CONSTANT, ROTATION_CONSTANT, false); break; case 1:
@@ -196,31 +189,45 @@ public class Robot extends IterativeRobot {
 		 * default: break; }
 		 */
 
-		// sd.driveMecanum(movementController, TRANSLATION_CONSTANT,
-		// ROTATION_CONSTANT, false);
+		sd.driveMecanum(movementController, TRANSLATION_CONSTANT,
+				ROTATION_CONSTANT, false);
 
 		intakeMultiplier = SmartDashboard.getNumber("Intake Speed");
-		// intake.setOuter(intakeController.getOuterIntake() *
-		// intakeMultiplier);
-		// intake.setInner(intakeController.getInnerIntake() *
-		// intakeMultiplier);
+		intake.setOuter(intakeController.getOuterIntake() * intakeMultiplier);
+		intake.setInner(intakeController.getInnerIntake() * intakeMultiplier);
 
-		// elevator.set(intakeController.getElevator());
+		elevator.set(intakeController.getElevator());
+		elevator.getRangeInches();
 
 		SmartDashboard.putNumber("X Axis", movementController.getX());
 		SmartDashboard.putNumber("Y Axis", movementController.getY());
 		SmartDashboard.putNumber("Z Axis", movementController.getZ());
 
-		SmartDashboard.putNumber("Ultrasonic test", rf.findRangeInches());
-
-		pr.println(movementController.getX() + "\t" + movementController.getY()
-				+ "\t" + movementController.getZ());
+		// Avoid sending commands to the robot too quickly
+		Timer.delay(timerDelay);
 	}
 
 	/**
 	 * This function is called periodically during test mode
 	 */
 	public void testPeriodic() {
+		checkToReapplyProperties();
+	}
+
+	private void checkToReapplyProperties() {
+
+		if (SmartDashboard.getBoolean("Reapply Properties")) {
+			try {
+				applyProperties(sr.readProperties(PROP_FILE_URL));
+				SmartDashboard.putString("Properties Loaded", "successfully");
+			} catch (IOException e) {
+				SmartDashboard.putString("Properties Loaded", "unsuccessfully");
+			} finally {
+				SmartDashboard.putBoolean("Reapply Properties", false);
+			}
+
+		}
+
 	}
 
 	/**
@@ -232,7 +239,8 @@ public class Robot extends IterativeRobot {
 	 */
 	private void applyProperties(Properties prop) {
 		// TODO: return to default ports
-		// Port
+
+		// TalonSR and TalonSRX ports
 		frontLeft = Integer.parseInt(prop.getProperty("talonFrontLeft"));
 		backLeft = Integer.parseInt(prop.getProperty("talonBackLeft"));
 		frontRight = Integer.parseInt(prop.getProperty("talonFrontRight"));
@@ -244,17 +252,18 @@ public class Robot extends IterativeRobot {
 		intakeOuterRight = Integer.parseInt(prop
 				.getProperty("intakeOuterRight"));
 
-		us = new Ultrasonic(1, 0);
-		us.setEnabled(true);
+		// Range finder port
+		rfPort = Integer.parseInt(prop.getProperty("rfPort"));
 
-		// initialize camera
+		// Initialize camera
 		cameraQuality = Integer.parseInt(prop.getProperty("cameraQuality"));
 		cameraPort1 = prop.getProperty("cameraID");
 
-		driveMode = Integer.parseInt(prop.getProperty("driveMode"));
-
-		// auton properties
+		// Auton properties
 		autonMode = Integer.parseInt(prop.getProperty("autonMode"));
+
+		// Teleop control properties
+		driveMode = Integer.parseInt(prop.getProperty("driveMode"));
 
 		if (driveMode == LOGITECH_DRIVE) {
 			intakeController = xbox;
@@ -264,7 +273,9 @@ public class Robot extends IterativeRobot {
 			movementController = xbox;
 		}
 
-		rf = new RangeFinder(Integer.parseInt(prop.getProperty("rfPort")));
+		// Timer delay
+		timerDelay = Double.parseDouble(prop.getProperty("timerDelay"));
+		SmartDashboard.putNumber("Timer Delay", timerDelay);
 
 		SmartDashboard.putString("Properties Loaded", "successfully");
 	}
